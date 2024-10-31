46cafed978dd9c44fa59a847348cd5142cdc9c3f
Alex <hohokoonalex@gmail.com>
2024-10-31 15:08:02 +0200

    player test

--- MCPProject/src/Main.cpp
+++ MCPProject/src/Main.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
-#include "GameObject.h"
 #include <cstdint>
+
+#include "GameObject.h"
 #include "Player.h"
 
 static constexpr uint32_t initialWindowWidth = 800;
@@ -13,15 +14,10 @@ int main()
 	sf::Texture texture;
 	texture.loadFromFile("res/albedo.png");
 
-	GameObject gameObject({ initialWindowWidth / 2.0f, initialWindowHeight / 2.0f }, texture, { 0.0f, 0.0f });
-	gameObject.setSize({ 100.0f, 100.0f });
-
-	constexpr float circleRadius = 100.0f;
-    sf::CircleShape circle(circleRadius);
-	circle.setPosition(initialWindowWidth / 2.0f - circleRadius, initialWindowHeight / 2.0f - circleRadius);
-    circle.setFillColor(sf::Color::Blue);
-
-    sf::Clock clock;
+    Player player({ initialWindowWidth / 2.0f, initialWindowHeight / 2.0f }, texture, { 0.0f, 0.0f });
+    player.setSize({ 100.0f , 100.0f });
+        
+	
     while (window.isOpen())
     {
         sf::Event event;
@@ -29,16 +25,13 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+            player.movePlayer(event);
         }
 
-        window.clear(sf::Color(255, 100, 100, 255));
-        
-		float currentTime = clock.getElapsedTime().asSeconds();
-        circle.setPosition(circle.getPosition().x + cos(currentTime) * 0.01
-                          , circle.getPosition().y + sin(currentTime) * 0.01);
 
-        window.draw(circle);
-		window.draw(gameObject);
+        window.clear(sf::Color(255, 100, 100, 255));
+
+        window.draw(player);
 
         window.display();
     }
