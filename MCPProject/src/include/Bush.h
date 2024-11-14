12c0db67acba51ce254482853b99868326ac620d
Andrei Moroianu <madalin.moroianu@student.unitbv.ro>
2024-11-14 12:15:52 +0200

    Added Bush class

---
+++ MCPProject/src/include/Bush.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "SFML/Graphics.hpp"
+#include "GameObject.h"
+
+class Bush : public GameObject
+{
+public:
+    Bush(
+        sf::Vector2f pos,
+        const sf::Texture& texture,
+        sf::Vector2f size = sf::Vector2f(40.0f, 40.0f)
+    );
+
+    bool isPassable() const;
+    sf::FloatRect getBounds() const;
+
+    bool operator==(const Bush& other) const;
+
+private:
+    bool m_isPassable{ true }; // Bushurile sunt traversabile
+};
