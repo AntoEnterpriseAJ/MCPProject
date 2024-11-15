2273efbe46b87014645af1516713ad21cfc2e8c2
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2024-11-15 14:26:46 +0200

    fixed the collisions

--- MCPProject/src/include/BulletManager.h
+++ MCPProject/src/include/BulletManager.h
@@ -12,8 +12,8 @@ public:
     void draw(sf::RenderWindow& window) const;
 
 private:
-    // TODO: Repair the collision cause the merge broke it
     void handleCollisions(std::vector<std::variant<Brick, Bush>>& level);
+    void removeInactive(std::vector<std::variant<Brick, Bush>>& level);
 private:
     std::vector<Bullet> m_bullets;
 };
