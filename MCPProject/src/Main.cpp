f213e00282d40968816363f95d7a2d5090d91338
Alex <hohokoonalex@gmail.com>
2024-11-19 21:47:44 +0200

    deleted unecessary code form main

--- MCPProject/src/Main.cpp
+++ MCPProject/src/Main.cpp
@@ -37,27 +37,4 @@ int main() {
 
     return 0;
 }
-    //if (bulletBounds.intersects(brick.getBounds()))
-        //        {
-        //            float explosionX = bullet.getPosition().x;
-        //            float explosionY = bullet.getPosition().y;
-
-        //            explosions.emplace_back(explosionX, explosionY, loadExplosionFrames("res/explosion.png", 32, 32, 16));
-        //            bullet.setInactive();
-    //float deltaTime = clock.restart().asSeconds();
-
-        //for (auto& explosion : explosions)
-        //{
-        //    explosion.update(deltaTime);
-        //}
-
-        //explosions.erase(std::remove_if(explosions.begin(), explosions.end(),
-        //    [](const Explosion& explosion) { return explosion.hasFinished(); }),
-        //    explosions.end());
-
-
-        //for (const auto& explosion : explosions)
-        //{
-        //    explosion.draw(window);
-        //}
-
+  
