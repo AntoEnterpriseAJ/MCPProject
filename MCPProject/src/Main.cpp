e5dc247516e52ba28d4e6a9f15e665759a9dbf68
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2024-11-25 18:30:39 +0200

    cleaned main

--- MCPProject/src/Main.cpp
+++ MCPProject/src/Main.cpp
@@ -1,36 +1,7 @@
-﻿#include <SFML/Graphics.hpp>
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include "Player.h"
-#include "Bullet.h"
-#include "Level.h"
-#include "Explosion.h"
-#include "Game.h"
+﻿#include "Game.h"
 
-std::vector<sf::Texture> loadExplosionFrames(const std::string& filePath, int frameWidth, int frameHeight, int numFrames)
+int main()
 {
-    sf::Image image;
-    if (!image.loadFromFile(filePath))
-    {
-        std::cerr << "ERROR: Unable to load image!" << std::endl;
-        return {};
-    }
-
-    std::vector<sf::Texture> textures(numFrames);
-    for (int i = 0; i < numFrames; ++i)
-    {
-        sf::Texture texture;
-        sf::IntRect rect((i % 4) * frameWidth, (i / 4) * frameHeight, frameWidth, frameHeight);
-        texture.loadFromImage(image, rect);
-        textures[i] = texture;
-    }
-
-    return textures;
-}
-
-int main() {
-
     Game game;
 
     game.render();
