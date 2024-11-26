a2e4e907839e198691bde16a11aa9d24e10be78a
Andrei Moroianu <madalin.moroianu@student.unitbv.ro>
2024-11-26 15:06:33 +0200

    Moved menu

---
+++ MCPProject/src/Menu.cpp
@@ -0,0 +1,40 @@
+#include "Menu.h"
+
+Menu::Menu(float width, float height) {
+    if (!font.loadFromFile("res/Jaro/Jaro-Regular-VariableFont_opsz.ttf")) {
+        // ERROR
+    }
+
+    startButton.setFont(font);
+    startButton.setCharacterSize(50);
+    startButton.setString("Start Game");
+    startButton.setPosition(width / 2 - startButton.getLocalBounds().width / 2, height / 2 - 100);
+
+    exitButton.setFont(font);
+    exitButton.setCharacterSize(50);
+    exitButton.setString("Exit");
+    exitButton.setPosition(width / 2 - exitButton.getLocalBounds().width / 2, height / 2 + 50);
+}
+
+void Menu::draw(sf::RenderWindow& window) {
+    window.draw(startButton);
+    window.draw(exitButton);
+}
+
+void Menu::handleInput(sf::RenderWindow& window) {
+    sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+
+    if (startButton.getGlobalBounds().contains(mousePos.x, mousePos.y) &&
+        sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+        startGame = true;
+    }
+
+    if (exitButton.getGlobalBounds().contains(mousePos.x, mousePos.y) &&
+        sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+        window.close();
+    }
+}
+
+bool Menu::isStartGame() const {
+    return startGame;
+}
---
+++ MCPProject/src/include/Menu.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "GameObject.h"
+
+class Menu {
+public:
+    Menu(float width, float height);
+
+    void draw(sf::RenderWindow& window);
+
+    void handleInput(sf::RenderWindow& window);
+
+    bool isStartGame() const;
+
+private:
+    sf::Font font;
+    sf::Text startButton;
+    sf::Text exitButton;
+    bool startGame = false;
+};
