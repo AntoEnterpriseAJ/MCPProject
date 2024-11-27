710c1b6cbcf84268fe7ada8129dec22e60172132
Alex <hohokoonalex@gmail.com>
2024-11-27 21:39:37 +0200

    changed menu to be implemented in game

--- MCPProject/src/Menu.cpp
+++ MCPProject/src/Menu.cpp
@@ -1,15 +1,14 @@
 ﻿#include "Menu.h"
 #include <iostream>
 
-Menu::Menu(float width, float height) 
+Menu::Menu(float width, float height)
 {
-    if (!backgroundTexture.loadFromFile("res/textures/background_menu.png")) 
+    if (!backgroundTexture.loadFromFile("res/textures/background_menu.png"))
     {
-        std::cerr << "Nu s-a putut incarca imaginea de fundal!" << std::endl;
+        std::cerr << "Failed to load background image!" << std::endl;
     }
 
     backgroundSprite.setTexture(backgroundTexture);
-
     backgroundSprite.setScale(
         static_cast<float>(width) / backgroundTexture.getSize().x,
         static_cast<float>(height) / backgroundTexture.getSize().y
@@ -17,7 +16,7 @@ Menu::Menu(float width, float height)
 
     if (!font.loadFromFile("res/font_text/Jaro-Regular-VariableFont_opsz.ttf"))
     {
-        std::cerr << "Nu s-a putut incarca font-ul!" << std::endl;
+        std::cerr << "Failed to load font!" << std::endl;
     }
 
     startButton.setFont(font);
@@ -31,32 +30,32 @@ Menu::Menu(float width, float height)
     exitButton.setPosition(width / 2 - exitButton.getLocalBounds().width / 2, height / 2 + 50);
 }
 
-void Menu::draw(sf::RenderWindow& window) 
+void Menu::draw(sf::RenderWindow& window) const
 {
     window.draw(backgroundSprite);
-
     window.draw(startButton);
     window.draw(exitButton);
 }
 
-void Menu::handleInput(sf::RenderWindow& window) 
+void Menu::handleInput(sf::RenderWindow& window)
 {
-    sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+    sf::Vector2f mousePos = static_cast<sf::Vector2f>(sf::Mouse::getPosition(window));
 
-    if (startButton.getGlobalBounds().contains(mousePos.x, mousePos.y) &&
+    if (startButton.getGlobalBounds().contains(mousePos) &&
         sf::Mouse::isButtonPressed(sf::Mouse::Left))
     {
-        startGame = true;
+        startGameSelected = true;
     }
 
-    if (exitButton.getGlobalBounds().contains(mousePos.x, mousePos.y) &&
-        sf::Mouse::isButtonPressed(sf::Mouse::Left)) 
+    if (exitButton.getGlobalBounds().contains(mousePos) &&
+        sf::Mouse::isButtonPressed(sf::Mouse::Left))
     {
         window.close();
     }
 }
 
-bool Menu::isStartGame() const 
+
+bool Menu::isStartGameSelected() const
 {
-    return startGame;
+    return startGameSelected;
 }
--- MCPProject/src/include/Menu.h
+++ MCPProject/src/include/Menu.h
@@ -1,21 +1,21 @@
 #pragma once
-#include "GameObject.h"
+#include <SFML/Graphics.hpp>
 
-class Menu {
+class Menu
+{
 public:
     Menu(float width, float height);
 
-    void draw(sf::RenderWindow& window);
-
+    void draw(sf::RenderWindow& window) const;
     void handleInput(sf::RenderWindow& window);
 
-    bool isStartGame() const;
+    bool isStartGameSelected() const;
 
 private:
     sf::Font font;
     sf::Text startButton;
     sf::Text exitButton;
-    bool startGame = false;
+    bool startGameSelected = false;
 
     sf::Texture backgroundTexture;
     sf::Sprite backgroundSprite;
