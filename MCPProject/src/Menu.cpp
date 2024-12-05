de8be9e9d487c1ab9c9ea032770faf0713cefe3e
Andrei Moroianu <madalin.moroianu@student.unitbv.ro>
2024-12-05 14:42:13 +0200

    Delete the logic for Login and Register

--- MCPProject/src/Menu.cpp
+++ MCPProject/src/Menu.cpp
@@ -14,77 +14,48 @@ Menu::Menu(float width, float height)
         static_cast<float>(height) / backgroundTexture.getSize().y
     );
 
-    if (!m_font.loadFromFile("res/font_text/Jaro-Regular-VariableFont_opsz.ttf"))
+    if (!font.loadFromFile("res/font_text/Jaro-Regular-VariableFont_opsz.ttf"))
     {
         std::cerr << "Failed to load font!" << std::endl;
     }
 
-    m_startButton.setFont(m_font);
-    m_startButton.setCharacterSize(50);
-    m_startButton.setString("Start Game");
-    m_startButton.setPosition(width / 2 - m_startButton.getLocalBounds().width / 2, height / 2 - 150);
+    startButton.setFont(font);
+    startButton.setCharacterSize(50);
+    startButton.setString("Start Game");
+    startButton.setPosition(width / 2 - startButton.getLocalBounds().width / 2, height / 2 - 100);
 
-    m_exitButton.setFont(m_font);
-    m_exitButton.setCharacterSize(50);
-    m_exitButton.setString("Exit");
-    m_exitButton.setPosition(width / 2 - m_exitButton.getLocalBounds().width / 2, height / 2 + 150);
-
-    m_loginButton.setFont(m_font);
-    m_loginButton.setCharacterSize(50);
-    m_loginButton.setString("Login");
-    m_loginButton.setPosition(width / 2 - m_loginButton.getLocalBounds().width / 2, height / 2 - 50);
-
-    m_registerButton.setFont(m_font);
-    m_registerButton.setCharacterSize(50);
-    m_registerButton.setString("Register");
-    m_registerButton.setPosition(width / 2 - m_registerButton.getLocalBounds().width / 2, height / 2 + 50);
+    exitButton.setFont(font);
+    exitButton.setCharacterSize(50);
+    exitButton.setString("Exit");
+    exitButton.setPosition(width / 2 - exitButton.getLocalBounds().width / 2, height / 2 + 50);
 }
 
-void Menu::draw(sf::RenderWindow& window)
+void Menu::draw(sf::RenderWindow& window) const
 {
     window.draw(backgroundSprite);
-    window.draw(m_startButton);
-    window.draw(m_exitButton);
-    window.draw(m_loginButton);
-    window.draw(m_registerButton);
+    window.draw(startButton);
+    window.draw(exitButton);
 }
 
 void Menu::handleInput(sf::RenderWindow& window)
 {
     sf::Vector2f mousePos = static_cast<sf::Vector2f>(sf::Mouse::getPosition(window));
 
-    if (m_startButton.getGlobalBounds().contains(mousePos) && sf::Mouse::isButtonPressed(sf::Mouse::Left))
+    if (startButton.getGlobalBounds().contains(mousePos) &&
+        sf::Mouse::isButtonPressed(sf::Mouse::Left))
     {
-        m_startGameSelected = true;
+        startGameSelected = true;
     }
 
-    if (m_exitButton.getGlobalBounds().contains(mousePos) && sf::Mouse::isButtonPressed(sf::Mouse::Left))
+    if (exitButton.getGlobalBounds().contains(mousePos) &&
+        sf::Mouse::isButtonPressed(sf::Mouse::Left))
     {
         window.close();
     }
-
-    if (m_loginButton.getGlobalBounds().contains(mousePos) && sf::Mouse::isButtonPressed(sf::Mouse::Left))
-    {
-        m_loginSelected = true;
-    }
-
-    if (m_registerButton.getGlobalBounds().contains(mousePos) && sf::Mouse::isButtonPressed(sf::Mouse::Left))
-    {
-        m_registerSelected = true;
-    }
 }
+
 
 bool Menu::isStartGameSelected() const
 {
-    return m_startGameSelected;
-}
-
-bool Menu::isLoginSelected() const
-{
-    return m_loginSelected;
-}
-
-bool Menu::isRegisterSelected() const
-{
-    return m_registerSelected;
+    return startGameSelected;
 }
--- MCPProject/src/include/Menu.h
+++ MCPProject/src/include/Menu.h
@@ -6,24 +6,17 @@ class Menu
 public:
     Menu(float width, float height);
 
-    void draw(sf::RenderWindow& window);
+    void draw(sf::RenderWindow& window) const;
     void handleInput(sf::RenderWindow& window);
 
     bool isStartGameSelected() const;
-    bool isLoginSelected() const;
-    bool isRegisterSelected() const;
 
 private:
-    sf::Font m_font;
-    sf::Text m_startButton;
-    sf::Text m_exitButton;
-    sf::Text m_loginButton;
-    sf::Text m_registerButton;
+    sf::Font font;
+    sf::Text startButton;
+    sf::Text exitButton;
+    bool startGameSelected = false;
 
     sf::Texture backgroundTexture;
     sf::Sprite backgroundSprite;
-
-    bool m_startGameSelected = false;
-    bool m_loginSelected = false;
-    bool m_registerSelected = false;
 };
