e443779f407749f359330788b55350b38d378ce0
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2024-12-08 01:53:21 +0200

    gameObject base class

---
+++ MCPProject/Client/src/include/GameObject.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "SFML/Graphics.hpp"
+#include <string>
+
+class GameObject : public sf::Drawable
+{
+public:
+	GameObject() = default;
+
+	GameObject(
+		sf::Vector2f       pos, 
+		const sf::Texture& texture, 
+		sf::Vector2f       size, 
+		sf::Vector2f       vel = { 0.0f, 0.0f }
+	);
+
+	~GameObject() override = default;
+
+	void setPosition(sf::Vector2f pos);
+	void setSize    (sf::Vector2f size);
+	void setVelocity(sf::Vector2f vel);
+	void setTexture (const sf::Texture& texture);
+
+	bool isInArea(
+		const sf::Vector2f& upLeft,
+		const sf::Vector2f& downRight
+	);
+
+	sf::FloatRect getBounds()   const noexcept;
+	sf::Vector2f  getPosition() const noexcept;
+	sf::Vector2f  getSize()	    const noexcept;
+	sf::Vector2f  getVelocity() const noexcept;
+	sf::Sprite    getSprite()   const noexcept;
+
+	void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+
+protected:
+	sf::Vector2f m_velocity;
+	sf::Sprite	 m_sprite;
+};
