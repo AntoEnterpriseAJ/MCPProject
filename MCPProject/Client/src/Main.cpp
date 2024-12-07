7f369bbd0f5e6a0f2ec65d1b440089e48cad825d
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2024-12-07 21:02:02 +0200

    send request, handle response

--- MCPProject/Client/src/Main.cpp
+++ MCPProject/Client/src/Main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <cpr/cpr.h>
 
 int main()
 {
-    std::cout << "Client\n";
+    cpr::Response greetResponse = cpr::Get(cpr::Url{"http://localhost:18080/greet"});
+
+    auto [statusCode, text] = std::tie(greetResponse.status_code, greetResponse.text);
+    if (statusCode == cpr::status::HTTP_OK)
+    {
+        std::cout << "GET was succesful\n";
+        std::cout << text << "\n";
+    }
+    else
+    {
+        std::cout << "GET failed, error: " << greetResponse.status_code << "\n";
+    }
 
     return 0;
 }
