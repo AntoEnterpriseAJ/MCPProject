acc9a49ba4e4cc8a31031cc5a669cc0ae5802a68
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2024-12-12 13:56:14 +0200

    server-side player data class

--- MCPProject/Server/src/Player.cpp
+++ MCPProject/Server/src/Player.cpp
@@ -1,10 +1,15 @@
 #include "Player.h"
 
-Player::Player(Position position)
-    : m_position(position)
+Player::Player(const Position& position, uint16_t id)
+    : m_position(position), m_id{id}
 {}
 
 Player::Position Player::getPosition() const
 {
     return m_position;
+}
+
+uint16_t Player::getID() const
+{
+    return m_id;
 }
--- MCPProject/Server/src/include/Player.h
+++ MCPProject/Server/src/include/Player.h
@@ -4,12 +4,14 @@
 class Player
 {
 public:
-    using Position = std::pair<int, int>;
+    using Position = std::pair<uint16_t, uint16_t>;
 
-    Player(Position position);
+    Player(const Position& position, uint16_t id);
 
     Position getPosition() const;
+    uint16_t getID() const;
 
 private:
     Position m_position;
+    uint16_t m_id;
 };
