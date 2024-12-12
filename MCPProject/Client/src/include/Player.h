35639ab342a06169798ccf9bb564eac78b59bacc
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2024-12-12 14:00:12 +0200

    client Player only has what's needed for rendering

--- MCPProject/Client/src/include/Player.h
+++ MCPProject/Client/src/include/Player.h
@@ -8,21 +8,14 @@ class Player : public GameObject
 public:
     Player() = default;
     Player(sf::Vector2f pos, const sf::Texture& texture, sf::Vector2f size);
+    ~Player() override = default;
 
     Direction getDirection() const;
-
-    void update(float deltaTime);
-    bool canShoot() const;
-    void restartCooldown();
-    
-    //bool canMove(const Level& level, float deltaTime);
-    //void movePlayer(const Level& level, float deltaTime);
 
 private:
     static constexpr float kCooldownTime = 0.5f;
     static constexpr float kPlayerSpeed = 150.0f;
 private:
     float     m_health;
-    float     m_cooldownDuration;
     Direction m_direction : 2;
 };
