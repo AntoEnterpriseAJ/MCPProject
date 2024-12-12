7a02beadefeb47b77339a1943a44c177f9b69dbd
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2024-12-12 14:00:34 +0200

    ResourceManager class

---
+++ MCPProject/Client/src/ResourceManager.cpp
@@ -0,0 +1,46 @@
+#include "ResourceManager.h"
+#include <iostream>
+
+ResourceManager& ResourceManager::getInstance()
+{
+    static ResourceManager resourceManager;
+    return resourceManager;
+}
+
+void ResourceManager::loadTextureFromFile(std::string_view path, std::string_view name)
+{
+    if (m_textures.contains(name))
+    {
+        std::cerr << "WARNING::ResourceManager::loadTexture: the texture was already loaded" << std::endl;
+        return;
+    }
+
+    sf::Texture texture;
+    if (!texture.loadFromFile(path.data()))
+    {
+        std::cerr << "ERROR::ResourceManager::loadTexture: can't find file at " << path << std::endl;
+        exit(-1);
+    }
+
+    m_textures[name] = texture;
+}
+
+const sf::Texture& ResourceManager::getTexture(std::string_view name) const
+{
+    if (!m_textures.contains(name))
+    {
+        std::cerr << "ERROR::ResourceManager::getTexture: texture " << name << " not found" << std::endl;
+        exit(-1);
+    }
+
+    return m_textures.at(name);
+}
+
+void ResourceManager::loadMusicFromFile(std::string_view path, sf::Music& music)
+{
+    if (!music.openFromFile(path.data()))
+    {
+        std::cerr << "ERROR::ResourceManager::loadMusic: Failed to load music file from " << path << std::endl;
+        exit(-1);
+    }
+}
---
+++ MCPProject/Client/src/include/ResourceManager.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <string_view>
+#include <unordered_map>
+#include <memory>
+
+class ResourceManager
+{
+public:
+    static ResourceManager& getInstance();
+
+    void loadTextureFromFile(std::string_view path, std::string_view name);
+    [[nodiscard]] const sf::Texture& getTexture(std::string_view name) const;
+
+    void loadMusicFromFile(std::string_view path, sf::Music& music);
+
+private:
+    ResourceManager() = default;
+
+private:
+    std::unordered_map<std::string_view, sf::Texture> m_textures;
+};
