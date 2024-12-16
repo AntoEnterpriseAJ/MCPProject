0db4aea87b14bf0e882cda1b5fa6abfcf683eb6f
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2024-12-16 16:30:28 +0200

    getRotation

--- MCPProject/Client/src/include/Direction.h
+++ MCPProject/Client/src/include/Direction.h
@@ -9,6 +9,22 @@ enum class Direction : uint8_t
     Right
 };
 
+// TODO: maybe namespace it
+constexpr float getRotation(Direction direction)
+{
+    switch (direction)
+    {
+    case Direction::Up:
+        return 0.0f;
+    case Direction::Down:
+        return 180.0f;
+    case Direction::Left:
+        return 270.0f;
+    case Direction::Right:
+        return 90.0f;
+    }
+}
+
 NLOHMANN_JSON_SERIALIZE_ENUM(Direction, {
     {Direction::Up, "up"},
     {Direction::Down, "down"},
