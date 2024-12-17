67c673734b3709594096677cabbaae7755b77a8e
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2024-12-17 16:06:03 +0200

    added room support

--- MCPProject/Client/src/NetworkManager.cpp
+++ MCPProject/Client/src/NetworkManager.cpp
@@ -1,27 +1,63 @@
 #include "NetworkManager.h"
 #include <cpr/cpr.h>
 #include <iostream>
+#include <format>
 
 const std::string NetworkManager::kServerUrl{"http://127.0.0.1:18080"};
 
 //TODO: add a logger
-//TODO: don't block the main thread waiting for responses
+//TODO: don't block the main thread waiting for responses (for now it's fine)
 //TODO: add custom json serialization for custom types
 NetworkManager::NetworkManager()
-    : m_clientVersion{0}
+    : m_clientVersion{0}, m_URL{kServerUrl}, m_currentRoomID{0}
 {}
 
-nlohmann::json NetworkManager::join(const Player& player)
+void NetworkManager::createRoom()
 {
-    nlohmann::json data = {
+    cpr::Response createResponse = cpr::Post(
+        cpr::Url{m_URL.addToPath("createRoom").build()}
+    );
+
+    if (createResponse.status_code != cpr::status::HTTP_OK)
+    {
+        std::cout << std::format("There was an error joining. HTTP status: {}, Error: {}\n"
+                                , createResponse.status_code
+                                , createResponse.error.message);
+    }
+
+    try
+    {
+        nlohmann::json response = nlohmann::json::parse(createResponse.text);
+
+        if (!response.contains("roomID"))
+        {
+            std::cout << "Invalid join response format: missing 'roomID'\n";
+            return;
+        }
+
+        m_currentRoomID = response["roomID"];
+        std::cout << std::format("Created room {}\n", response["roomID"].get<uint8_t>());
+    }
+    catch (const nlohmann::json::parse_error& error)
+    {
+        std::cout << std::format("Error parsing createRoom response: {}\n", error.what());
+    }
+}
+
+nlohmann::json NetworkManager::join(const Player& player, uint8_t roomID)
+{
+    std::cout << "Trying to join...\n";
+    nlohmann::json data {
         {"position", {player.getPosition().x, player.getPosition().y}}
     };
 
     cpr::Response joinResponse = cpr::Post(
-        cpr::Url{kServerUrl + "/join"},
+        cpr::Url{m_URL.addToPath("joinRoom").addToPath(std::to_string(roomID)).build()},
         cpr::Header{{"Content-Type", "application/json"}},
         cpr::Body{data.dump()}
     );
+    
+    std::cout << m_URL.addToPath("joinRoom").addToPath(std::to_string(roomID)).build() << "\n";
 
     if (joinResponse.status_code != cpr::status::HTTP_OK)
     {
@@ -35,13 +71,14 @@ nlohmann::json NetworkManager::join(const Player& player)
     {
         nlohmann::json response = nlohmann::json::parse(joinResponse.text);
 
-        if (!response.contains("id"))
+        if (!response.contains("playerID"))
         {
-            std::cout << "Invalid join response format: missing 'id'\n";
+            std::cout << "Invalid join response format: missing 'playerID'\n";
             return {};
         }
 
         std::cout << response["message"] << "\n";
+        m_currentRoomID = roomID;
         return response;
     }
     catch (const nlohmann::json::parse_error& error)
@@ -51,10 +88,11 @@ nlohmann::json NetworkManager::join(const Player& player)
     }
 }
 
-//TODO: maybe us std::optional and check in Game to see if the response is empty. nlohmann::json foo.empty() works aswell
 nlohmann::json NetworkManager::update()
 {
-    m_session.SetUrl(cpr::Url{kServerUrl + "/gameState"});
+    m_session.SetUrl(
+        cpr::Url{m_URL.addToPath("room").addToPath(std::to_string(m_currentRoomID)).addToPath("gameState").build()}
+    );
     m_session.SetParameters(cpr::Parameters{{"clientVersion", std::to_string(m_clientVersion)}});
 
     cpr::Response response = m_session.Get();
@@ -79,11 +117,11 @@ nlohmann::json NetworkManager::update()
 
         if (updateResponse["serverVersion"] == m_clientVersion)
         {
-            std::cout << std::format("No update needed, clientVersion==serverVersion=={}\n", m_clientVersion);
+            //std::cout << std::format("No update needed, clientVersion==serverVersion=={}\n", m_clientVersion);
             return {};
         }
 
-        std::cout << "Update needed\n";
+        //std::cout << "Update needed\n";
         m_clientVersion = updateResponse["serverVersion"];
         return updateResponse;
     }
@@ -91,6 +129,29 @@ nlohmann::json NetworkManager::update()
     {
         std::cout << std::format("Error parsing join response: {}\n", error.what());
         return {};
+    }
+}
+
+nlohmann::json NetworkManager::getExistingRooms()
+{
+    cpr::Response response = cpr::Get(
+        cpr::Url{m_URL.addToPath("getRooms").build()}
+    );
+
+    if (response.status_code != cpr::status::HTTP_OK) {
+                std::cout << std::format("Couldn't retrieve the existings rooms. HTTP Status: {}, Error: {}\n"
+                                , response.status_code
+                                , response.error.message);
+        return {};
+    }
+
+    try
+    {
+        return nlohmann::json::parse(response.text);
+    }
+    catch (const nlohmann::json::parse_error& e)
+    {
+        throw std::runtime_error("JSON parse error: " + std::string(e.what()));
     }
 }
 
@@ -102,7 +163,9 @@ void NetworkManager::movePlayer(uint16_t clientID, Direction direction, float de
         {"deltaTime", deltaTime}
     };
 
-    m_session.SetUrl(cpr::Url{kServerUrl + "/move"});
+    m_session.SetUrl(
+        cpr::Url{m_URL.addToPath("room").addToPath(std::to_string(m_currentRoomID)).addToPath("move").build()}
+    );
     m_session.SetHeader(cpr::Header{{"Content-Type", "application/json"}});
     m_session.SetBody(cpr::Body{data.dump()});
 
@@ -116,5 +179,15 @@ void NetworkManager::movePlayer(uint16_t clientID, Direction direction, float de
         return;
     }
 
-    std::cout << response.text << "\n";
+    //std::cout << response.text << "\n";
+}
+
+void NetworkManager::setRoomID(uint8_t roomID)
+{
+    m_currentRoomID = roomID;
+}
+
+uint8_t NetworkManager::getCurrentRoomID() const
+{
+    return m_currentRoomID;
 }
--- MCPProject/Client/src/include/NetworkManager.h
+++ MCPProject/Client/src/include/NetworkManager.h
@@ -1,22 +1,29 @@
 #pragma once
 #include <Player.h>
 #include <cstdint>
-#include <string>
 #include <nlohmann/json.hpp>
 #include <cpr/cpr.h>
+#include "URL.h"
 
 class NetworkManager
 {
 public:
     NetworkManager();
 
-    nlohmann::json join(const Player& player);
+    void createRoom();
+    nlohmann::json join(const Player& player, uint8_t roomID);
     nlohmann::json update();
+    nlohmann::json getExistingRooms();
     void movePlayer(uint16_t clientID, Direction direction, float deltaTime);
+
+    void setRoomID(uint8_t roomID);
+    uint8_t getCurrentRoomID() const;
 
 private:
     static const std::string kServerUrl;
 private:
+    uint8_t      m_currentRoomID;
     cpr::Session m_session;
     uint32_t     m_clientVersion;
+    URL          m_URL;
 };
