09061a303529584a207eea3209c925dd242d8343
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2024-12-27 15:04:18 +0200

    new ObstacleType enum class

---
+++ MCPProject/Server/src/include/ObstacleType.h
@@ -0,0 +1,10 @@
+#pragma once
+
+enum class ObstacleType
+{
+    None             = 0,
+    Brick            = 1,
+    Bush             = 2,
+    UnbreakableBrick = 3,
+    BombBrick        = 4,
+};
