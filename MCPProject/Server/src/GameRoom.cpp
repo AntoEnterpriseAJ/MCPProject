61720c79b0f5f7462425d4462aeff20b5151e3a7
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2024-12-27 14:53:17 +0200

    collisions

--- MCPProject/Server/src/GameRoom.cpp
+++ MCPProject/Server/src/GameRoom.cpp
@@ -1,16 +1,19 @@
 #include "GameRoom.h"
+#include "ObstacleType.h"
+
+#include <iostream>
 
 GameRoom::GameRoom()
-    : m_idCounter{0}, m_roomID{0}, m_version{0}, m_players{}
+    : m_idCounter{ 0 }, m_roomID{ 0 }, m_version{ 0 }, m_players{}
 {}
 
 GameRoom::GameRoom(uint8_t m_roomID)
-    : m_roomID{m_roomID}, m_idCounter{0}, m_version{0}, m_players{}, m_level{}
+    : m_roomID{ m_roomID }, m_idCounter{ 0 }, m_version{ 0 }, m_players{}, m_level{}
 {
     m_level.load();
 }
 
-uint8_t GameRoom::addPlayer(const Player::Position& position)
+uint8_t GameRoom::addPlayer(const Vec2f& position)
 {
     m_players.emplace(m_idCounter, position);
     m_version = (m_version + 1) % kMaxVersion;
@@ -28,13 +31,13 @@ nlohmann::json GameRoom::getStateResponse(uint32_t clientVersion) const noexcept
         response["levelLayout"] = m_level.getLayout();
 
         response["players"] = nlohmann::json::array();
-        for (const auto& [id, player]: m_players)
+        for (const auto& [id, player] : m_players)
         {
             response["players"].push_back({
                 {"id", id},
                 {"position", {player.getPosition().x, player.getPosition().y}}, // TODO: custom serialization
                 {"direction", player.getDirection()}
-            });
+                });
         }
     }
 
@@ -43,11 +46,52 @@ nlohmann::json GameRoom::getStateResponse(uint32_t clientVersion) const noexcept
 
 void GameRoom::move(uint8_t playerID, Direction direction, float deltaTime)
 {
-    m_players[playerID].move(direction, deltaTime);
+    auto alignToGrid = [](float value, int gridSize) -> float {
+        return std::round(value / gridSize) * gridSize;
+        };
+
+    Player& player{ m_players.at(playerID) };
+    Vec2f newPosition{ player.getPosition() };
+
+    if (direction != player.getDirection())
+    {
+        if (direction == Direction::Up || direction == Direction::Down)
+        {
+            newPosition.x = alignToGrid(newPosition.x, Level::kGridSize / 2.0f);
+        }
+        else if (direction == Direction::Left || direction == Direction::Right)
+        {
+            newPosition.y = alignToGrid(newPosition.y, Level::kGridSize / 2.0f);
+        }
+    }
+
+    player.setDirection(direction);
+    if (direction == Direction::Up)
+    {
+        newPosition.y -= Player::kPlayerSpeed * deltaTime;
+    }
+    else if (direction == Direction::Down)
+    {
+        newPosition.y += Player::kPlayerSpeed * deltaTime;
+    }
+    else if (direction == Direction::Left)
+    {
+        newPosition.x -= Player::kPlayerSpeed * deltaTime;
+    }
+    else if (direction == Direction::Right)
+    {
+        newPosition.x += Player::kPlayerSpeed * deltaTime;
+    }
+
+    if (!checkCollision(player, newPosition))
+    {
+        player.setPosition(newPosition);
+    }
+
     m_version = (m_version + 1) % kMaxVersion;
 }
 
-Level GameRoom::getLevel() const noexcept
+const Level& GameRoom::getLevel() const noexcept
 {
     return m_level;
 }
@@ -65,4 +109,31 @@ uint8_t GameRoom::getID() const noexcept
 const std::unordered_map<uint8_t, Player>& GameRoom::getPlayers() const noexcept
 {
     return m_players;
+}
+
+bool GameRoom::checkCollision(const Player& player, const Vec2f& newPosition)
+{
+    Vec2f topLeft     { newPosition - player.getOrigin() };
+    Vec2f bottomRight { topLeft + player.getSize() };
+
+    int topLeftX     = static_cast<int>(std::floor(topLeft.x / Level::kGridSize));
+    int topLeftY     = static_cast<int>(std::floor(topLeft.y / Level::kGridSize));
+    int bottomRightX = static_cast<int>(std::floor(bottomRight.x / Level::kGridSize));
+    int bottomRightY = static_cast<int>(std::floor(bottomRight.y / Level::kGridSize));
+
+    for (int x = topLeftX; x <= bottomRightX; ++x)
+    {
+        for (int y = topLeftY; y <= bottomRightY; ++y)
+        {
+            const auto& currentObstacle {m_level[{y, x}]};
+            if (!currentObstacle) continue;
+
+            if (!currentObstacle->isPassable() && currentObstacle->collides(topLeft, bottomRight))
+            {
+                return true;
+            }
+        }
+    }
+
+    return false;
 }
--- MCPProject/Server/src/include/GameRoom.h
+++ MCPProject/Server/src/include/GameRoom.h
@@ -3,6 +3,7 @@
 #include <nlohmann/json.hpp>
 #include "Player.h"
 #include "Level.h"
+#include "Vec2f.h"
 
 class GameRoom
 {
@@ -10,15 +11,17 @@ public:
     GameRoom();
     GameRoom(uint8_t m_roomID);
 
-    uint8_t addPlayer(const Player::Position& position);
+    uint8_t addPlayer(const Vec2f& position);
     nlohmann::json getStateResponse(uint32_t clientVersion) const noexcept;
     void move(uint8_t playerID, Direction direction, float deltaTime);
 
-    Level getLevel() const noexcept;
+    const Level& getLevel() const noexcept;
     uint32_t getVersion() const noexcept;
     uint8_t getID() const noexcept;
     const std::unordered_map<uint8_t, Player>& getPlayers() const noexcept;
 
+private:
+    bool checkCollision(const Player& player, const Vec2f& newPosition);
 private:
     static constexpr uint32_t kMaxVersion{1000000};
 private:
