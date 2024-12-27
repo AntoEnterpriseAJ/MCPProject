7fc09a79fc5b13a077f4e2bbc4a1832300d410f8
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2024-12-27 14:52:33 +0200

    new server-side GameObject class

---
+++ MCPProject/Server/src/GameObject.cpp
@@ -0,0 +1,76 @@
+#include "GameObject.h"
+
+GameObject::GameObject(const Vec2f& position, const Vec2f& size)
+    : m_size{size}, m_origin{0.0f, 0.0f}, m_position{position}
+{}
+
+bool GameObject::collides(const GameObject& other) const noexcept
+{
+    Vec2f topLeft{this->getTopLeft()};
+    Vec2f bottomRight{this->getBottomRight()};
+    Vec2f otherTopLeft{other.getTopLeft()};
+    Vec2f otherBottomRight{other.getBottomRight()};
+
+    bool xCollision = otherBottomRight.x >= topLeft.x &&
+                      otherTopLeft.x <= bottomRight.x;
+
+    bool yCollision = otherBottomRight.y >= topLeft.y &&
+                      otherTopLeft.y <= bottomRight.y;
+
+    return xCollision && yCollision;
+}
+
+bool GameObject::collides(const Vec2f& otherTopLeft, const Vec2f& otherBottomRight) const noexcept
+{
+    Vec2f topLeft{this->getTopLeft()};
+    Vec2f bottomRight{this->getBottomRight()};
+
+    bool xCollision = otherBottomRight.x >= topLeft.x &&
+                      otherTopLeft.x <= bottomRight.x;
+
+    bool yCollision = otherBottomRight.y >= topLeft.y &&
+                      otherTopLeft.y <= bottomRight.y;
+
+    return xCollision && yCollision;
+}
+
+// TODO: implement
+void GameObject::rotate(float)
+{
+
+}
+
+void GameObject::setOrigin(const Vec2f& originOffset)
+{
+    m_origin = originOffset;
+}
+
+void GameObject::setPosition(const Vec2f& position)
+{
+    m_position = position;
+}
+
+Vec2f GameObject::getOrigin() const noexcept
+{
+    return m_origin;
+}
+
+Vec2f GameObject::getPosition() const noexcept
+{
+    return m_position;
+}
+
+Vec2f GameObject::getSize() const noexcept
+{
+    return m_size;
+}
+
+Vec2f GameObject::getTopLeft() const noexcept
+{
+    return m_position - m_origin;
+}
+
+Vec2f GameObject::getBottomRight() const noexcept
+{
+    return m_position - m_origin + m_size;
+}
---
+++ MCPProject/Server/src/include/GameObject.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <Vec2f.h>
+
+class GameObject
+{
+public:
+    GameObject(const Vec2f& topLeft, const Vec2f& size);
+    virtual ~GameObject() = default;
+
+    bool collides(const GameObject& other) const noexcept;
+    bool collides(const Vec2f& otherTopLeft, const Vec2f& otherBottomRight) const noexcept;
+
+    // TODO: implement
+    void rotate(float);
+    void setOrigin(const Vec2f& originOffset);
+    void setPosition(const Vec2f& position);
+
+    Vec2f getOrigin()      const noexcept;
+    Vec2f getPosition()    const noexcept;
+    Vec2f getSize()        const noexcept;
+    Vec2f getTopLeft()     const noexcept;
+    Vec2f getBottomRight() const noexcept;
+private:
+    Vec2f m_position;
+    Vec2f m_origin;
+    Vec2f m_size;
+};
