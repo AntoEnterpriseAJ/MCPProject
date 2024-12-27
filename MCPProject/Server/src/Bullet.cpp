f67ce81847ce129abb8157a496d98191b9c076b7
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2024-12-27 14:52:08 +0200

    new server-side Bullet class

---
+++ MCPProject/Server/src/Bullet.cpp
@@ -0,0 +1,38 @@
+﻿#include "Bullet.h"
+
+Bullet::Bullet(const Vec2f& pos, Direction dir, const Vec2f& size)
+    : GameObject{ pos, size }, m_state{State::Active}, m_direction{dir}
+{}
+
+void Bullet::update(float deltaTime)
+{
+    if (m_direction == Direction::Up)
+        this->move(0, -kBulletSpeed * deltaTime);
+    else if (m_direction == Direction::Down)
+        this->move(0, kBulletSpeed * deltaTime);
+    else if (m_direction == Direction::Left)
+        this->move(-kBulletSpeed * deltaTime, 0);
+    else if (m_direction == Direction::Right)
+        this->move(kBulletSpeed * deltaTime, 0);
+}
+
+void Bullet::setState(Bullet::State state)
+{
+    m_state = state;
+}
+
+void Bullet::move(float offsetX, float offsetY)
+{
+    this->setPosition(this->getPosition() + Vec2f{offsetX, offsetY});
+}
+
+Direction Bullet::getDirection() const
+{
+    return m_direction;
+}
+
+Bullet::State Bullet::getState() const
+{
+    return m_state;
+}
+
---
+++ MCPProject/Server/src/include/Bullet.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+#include "Direction.h"
+#include "Vec2f.h"
+#include "GameObject.h"
+
+class Bullet : public GameObject
+{
+public:
+    enum class State : uint8_t
+    {
+        Active,
+        Inactive,
+    };
+public:
+    Bullet(
+        const Vec2f& pos, 
+        Direction    dir,
+        const Vec2f& size = {kBulletSizeX, kBulletSizeY}
+    );
+    ~Bullet() override = default;
+
+    void update(float deltaTime);
+    void setState(State state);
+    void move(float offsetX, float offsetY);
+
+    Direction getDirection() const;
+    State getState() const;
+
+private:
+    static constexpr float kBulletSpeed { 400.0f };
+    static constexpr float kBulletSizeX { 20.0f };
+    static constexpr float kBulletSizeY { 50.0f };
+private:
+    Direction m_direction : 2;
+    State     m_state     : 1;
+};
