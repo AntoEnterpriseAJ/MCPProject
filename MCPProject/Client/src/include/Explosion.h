3a0474e09fae36469c2b9cf49b7273893efac856
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2025-01-12 23:50:23 +0200

    feat: client-side Explosion implementation

---
+++ MCPProject/Client/src/include/Explosion.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+class Explosion : public sf::Drawable
+{
+public:
+    Explosion(sf::Vector2f position, const sf::Texture& explosionSheet);
+
+    void update(float deltaTime);
+
+    void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
+    bool hasFinished() const;
+
+private:
+    void updateExplosionFrame();
+
+private:
+    sf::Sprite m_explosionSheet;
+
+    int   m_currentFrame;
+    float m_frameTime;
+    float m_elapsedTime;
+    bool  m_finished;
+};
