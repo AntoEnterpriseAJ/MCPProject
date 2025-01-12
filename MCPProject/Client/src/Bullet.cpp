ecf2dc88c2c83a78012e6c861066be45f1088fa8
Antonio Emanuel JELERIU <antonio.jeleriu@student.unitbv.ro>
2025-01-12 23:49:59 +0200

    feat: client-side Bullet implementation

---
+++ MCPProject/Client/src/Bullet.cpp
@@ -0,0 +1,50 @@
+﻿#include "Bullet.h"
+#include "Game.h"
+
+Bullet::Bullet(sf::Vector2f pos, const sf::Texture& texture, Direction dir, sf::Vector2f size)
+    : GameObject{ pos, texture, size }, m_state{State::Active}, m_direction{dir}
+{
+    m_sprite.setOrigin(m_sprite.getLocalBounds().width / 2, m_sprite.getLocalBounds().height / 2);
+
+    if (dir == Direction::Up)
+        m_sprite.setRotation(0.0f);
+    else if (dir == Direction::Down)
+        m_sprite.setRotation(180.0f);
+    else if (dir == Direction::Left)
+        m_sprite.setRotation(270.0f);
+    else if (dir == Direction::Right)
+        m_sprite.setRotation(90.0f);
+}
+
+void Bullet::update(float deltaTime)
+{
+    if (m_direction == Direction::Up)
+        this->move(0, -kBulletSpeed * deltaTime);
+    else if (m_direction == Direction::Down)
+        this->move(0, kBulletSpeed * deltaTime);
+    else if (m_direction == Direction::Left)
+        this->move(-kBulletSpeed * deltaTime, 0);
+    else if (m_direction == Direction::Right)
+        this->move(kBulletSpeed * deltaTime, 0);
+}
+
+void Bullet::setState(Bullet::State state)
+{
+    m_state = state;
+}
+
+void Bullet::move(float offsetX, float offsetY)
+{
+    m_sprite.move(offsetX, offsetY);
+}
+
+Direction Bullet::getDirection() const
+{
+    return m_direction;
+}
+
+Bullet::State Bullet::getState() const
+{
+    return m_state;
+}
+
---
+++ MCPProject/Client/src/include/Bullet.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+#include <SFML/Graphics.hpp>
+#include "Direction.h"
+#include "GameObject.h"
+
+class Bullet : public GameObject
+{
+public:
+    enum class State : uint8_t
+    {
+        Active,
+        Inactive,
+    };
+
+public:
+
+    Bullet(
+        sf::Vector2f       pos, 
+        const sf::Texture& texture, 
+        Direction,
+        sf::Vector2f       size = sf::Vector2f(kBulletSizeX, kBulletSizeY)
+    );
+
+    void update(float deltaTime);
+    void setState(State state);
+    void move(float offsetX, float offsetY);
+
+    Direction getDirection() const;
+    State getState() const;
+
+private:
+    static constexpr float kBulletSpeed { 400.0f };
+    static constexpr float kBulletSizeX { 50.0f };
+    static constexpr float kBulletSizeY { 50.0f };
+
+private:
+    Direction m_direction : 2;
+    State     m_state     : 1;
+};
